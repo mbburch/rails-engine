57cbfc94b153ac74e85883363d7d3ace0f7924bb
MB Burch <marybethburch@gmail.com>
2015-11-04 13:22:03 -0700

    add item bi. all needs refactoring from ruby to AR

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,24 @@ class Item < ActiveRecord::Base
   has_many :invoices, through: :invoice_items
 
   belongs_to :merchant
+
+  def self.most_revenue(quantity)
+    all.max_by(quantity.to_i) { |item| item.revenue[:revenue] }
+  end
+
+  def self.most_items(quantity)
+    all.max_by(quantity.to_i) { |item| item.item_quantity }
+  end
+
+  def revenue
+    { revenue: invoices.successful.sum("invoice_items.unit_price * invoice_items.quantity") }
+  end
+
+  def item_quantity
+    invoices.successful.sum("invoice_items.quantity")
+  end
+
+  def best_day
+    { best_day: invoices.successful.group('invoices.created_at').sum('quantity').sort_by { |key, val| val }.reverse.flatten.first }
+  end
 end
